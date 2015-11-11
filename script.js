bbd4e9170303df5edbd2998239ee63442d6620e4
omegadeep10 <omegadeep10@outlook.com>
2015-11-11 10:19:29 -0500

    Create screen event listener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//empty js file, add code here.
+//setup calcScreen variable
+var calcScreen = document.getElementById("screen");
+
+//event listener to calculate math if enter key is pressed
+calcScreen.addEventListener("keydown", function(key) {
+    if (key.keycode === 13 || key.which === 13) {
+        console.log(calcScreen.value);
+    }
+});
+
+
+//event listener to calculate math if "=" button is pressed
+
+
+//event listeners to watch for button presses and append the button value to screen
